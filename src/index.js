adb2665ff9065c2d051f8f8d4d8a4d5db203d578
JasminBower <jasmin.ibower@gmail.com>
2022-02-28 11:34:25 +0100

    refactored classes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 let bg;
 let corgiImg;
 let corgi;
+let teacup;
+let teacupImg;
 
 
 function preload(){
     bg = loadImage('../images/inside palace.jpeg');
     corgiImg = loadImage('../images/palaceCorgi.png');
+    teacupImg = loadImage('../images/teacup.png')
 };
 
 function setup(){
-    const canvas = createCanvas(1600, 850);
+    const canvas = createCanvas(1600, 700);
     canvas.parent('game-room');
-    corgi = new Dog(0, 0, 50, 50)
+    corgi = new Dog(560, 520, 125, 125);
+
    
 
 }
@@ -25,30 +29,36 @@ class GameObject{
 
     };
 
-    draw(){
-        image(img, this.x, this.y, this.width, this.height)
-    };
+    // draw(){
+    //     image(corgiImg, this.x, this.y, this.width, this.height)
+    // };
 
-    moveX(num){
-        this.x += num;
-    };
+    // moveX(num){
+    //     this.x += num;
+    // };
 
-    moveY(num){
-        this.y += num;
-    };
+    // moveY(num){
+    //     this.y += num;
+    // };
 }; 
 
 
 class Dog extends GameObject{
     constructor(x, y, width, height){
         super(x, y, width, height);
-        this.x = x;
-        this.y = y;
     }
 
     draw(){
-        image(corgiImg, this.x, this.y, width, height);
+        console.log(this.width, '<<<<<')
+        image(corgiImg, this.x, this.y, this.width, this.height);
     }
+      moveX(num){
+        this.x += num;
+    };
+
+    moveY(num){
+        this.y += num;
+    };
 };
 
 
@@ -59,4 +69,13 @@ class Dog extends GameObject{
 function draw(){
     background(bg)
     corgi.draw();
+     if(keyIsDown(LEFT_ARROW)){
+   corgi.moveX(-10)
+ } else if(keyIsDown(RIGHT_ARROW)){
+   corgi.moveX(10)
+ } else if(keyIsDown(UP_ARROW)){
+   corgi.moveY(-10)
+ } else if(keyIsDown(DOWN_ARROW)){
+   corgi.moveY(10)
+ };
 }
