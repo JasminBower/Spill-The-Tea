bd6340b0cefc621f48f63f8e2660c61bb1c54e90
JasminBower <jasmin.ibower@gmail.com>
2022-03-01 14:05:15 +0100

    able to rotate through pages

--- src/index.js
+++ src/index.js
@@ -5,21 +5,43 @@ let teacup;
 let teacupImg;
 
 
+
+const splashScreen = document.querySelector('#splashscreen');
+const gameScreen = document.querySelector('#game-screen');
+const gameOverScreen = document.querySelector('#game-over-screen');
+
+
+const startBtn = document.querySelector('#start-btn')
+
 function preload(){
     bg = loadImage('../images/inside palace.jpeg');
     corgiImg = loadImage('../images/palaceCorgi.png');
     teacupImg = loadImage('../images/teacup.png')
 };
 
+
 function setup(){
     const canvas = createCanvas(1600, 700);
-    canvas.parent('game-room');
+    canvas.parent('game-screen');
     corgi = new Dog(560, 520, 125, 125);
     teacup = new Tea(0, 0, 100, 100)
 
-   
+};
 
-}
+function draw(){
+    background(bg)
+    corgi.draw();
+    teacup.draw();
+     if(keyIsDown(LEFT_ARROW)){
+   corgi.moveX(-10)
+ } else if(keyIsDown(RIGHT_ARROW)){
+   corgi.moveX(10)
+ } else if(keyIsDown(UP_ARROW)){
+   corgi.moveY(-10)
+ } else if(keyIsDown(DOWN_ARROW)){
+   corgi.moveY(10)
+ };
+};
 
 class GameObject{
     constructor(x, y, width, height){
@@ -50,7 +72,7 @@ class Dog extends GameObject{
     }
 
     draw(){
-        console.log(this.width, '<<<<<')
+        //console.log(this.width, '<<<<<')
         image(corgiImg, this.x, this.y, this.width, this.height);
     }
       moveX(num){
@@ -74,20 +96,27 @@ class Tea extends GameObject{
 
 
 
+function gameStatus() {
+  
+};
+
+  window.addEventListener('load', () => {
+      console.log('in load');
+         //noCanvas()
+         splashScreen.style.display = 'flex';
+         gameScreen.style.display = 'none';
+         gameOverScreen.style.display = 'none';
+
+   
+        startBtn.addEventListener('click', () => {
+            console.log('am I clicking?')
+           splashScreen.style.display = 'none';
+            gameScreen.style.display = 'flex';
+            //gameOverScreen.style.display = 'none';
+        })
+    })
 
 
 
-function draw(){
-    background(bg)
-    corgi.draw();
-    teacup.draw();
-     if(keyIsDown(LEFT_ARROW)){
-   corgi.moveX(-10)
- } else if(keyIsDown(RIGHT_ARROW)){
-   corgi.moveX(10)
- } else if(keyIsDown(UP_ARROW)){
-   corgi.moveY(-10)
- } else if(keyIsDown(DOWN_ARROW)){
-   corgi.moveY(10)
- };
-}
+
+
